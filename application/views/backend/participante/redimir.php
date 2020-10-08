14d62a7034646ffa29a4305b592099795bac06a7
Bayron St <byr.garcia@gmail.com>
2020-10-07 22:36:10 -0500

    Update redimir.php
    Dev text

---
+++ application/views/backend/participante/redimir.php
@@ -0,0 +1,4 @@
+<h3 class="">Códigos para juegos Android</h3>
+<p style="font-size:20px;">Si tienes sufucientes créditos puedes usarlos para redimirlos por códigos y usarlos en nuestra app Store</p>
+<p style="font-size:20px;">Descargar nuetra app desde este link y sigue los pasos para ingresar tu código y disfrutar nuestros juegos.</p>
+<button class="btn btn-blue">Redimir Código</button>

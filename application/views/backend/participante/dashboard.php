0bcb3c4deba78cb7abd37134d9e83366f1ea687f
Bayron St <byr.garcia@gmail.com>
2020-10-08 23:55:27 -0500

    Modulo Perfil
    Se dan ajustes del modulo de perfil y registro

--- application/views/backend/participante/dashboard.php
+++ application/views/backend/participante/dashboard.php
@@ -1,113 +1,116 @@
 
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
 
-        <?php
-												$count=1;
-  									    $ID_PARTICIPANTE = $this->session->userdata('login_user_id');
+		<?php
+			$id_participante = $this->session->userdata('login_user_id');
+			$query = "SELECT SUM(cantidad) as CANTIDAD from creditos where ID_PARTICIPANTE = $id_participante";
+			$query2 = "SELECT SUM(cantidad) as CANTIDAD2 from creditos where ID_PARTICIPANTE = $id_participante AND TIPO = 'JUEGO'";
+			$query3 = "SELECT SUM(cantidad) as CANTIDAD3 from creditos where ID_PARTICIPANTE = $id_participante AND TIPO = 'FACTURA'";
+            $query4 = "SELECT sum(CANTIDAD) as CANTIDAD4 FROM canjes WHERE ID_PARTICIPANTE = $id_participante";
 
-												$query = "SELECT  SUM(cantidad) as CANTIDAD from creditos where ID_PARTICIPANTE = $ID_PARTICIPANTE ";
-												$query = $this->db->query($query);
-												$data = $query->result_array();
+			$query = $this->db->query($query);
+			$query2 = $this->db->query($query2);
+			$query3 = $this->db->query($query3);
+            $query4 = $this->db->query($query4);
+			
+			$data = $query->result_array();
+			$data2 = $query2->result_array();
+			$data3 = $query3->result_array();
+            $data4 = $query4->result_array();
+			
+			foreach ($data as $row) {$nCreditos = $row['CANTIDAD'];}
+			foreach ($data2 as $row2) {$nCreditos2 = $row2['CANTIDAD2'];}
+			foreach ($data3 as $row3) {$nCreditos3 = $row3['CANTIDAD3'];}
+			foreach ($data4 as $row4) {$nCreditos4 = $row4['CANTIDAD4'];}
+		
+		
+		?>
 
+		<div class="col-md-6 col-xs-12">
+		
 
-                        foreach($data as $row):
-                   ?>
-
-
-		<H2 align="center" style="color:blue">TOTAL CRÉDITOS ACUMULADOS</H2>
-
-			 <div class="col-sm-4 col-xs-6">
-          <div class="tile-stats tile-blue">
-            <div class="icon"><img src="<?php echo site_url('../uploads/imagen_creditos_color.png');?>"></div>
-          		<div class="num"><?php if($row['CANTIDAD']>0){echo $row['CANTIDAD'];} else echo '0';?></div>
-							<h3>TOTAL CREDITOS</h3>
-        </div>
-  		</div>
-
-<?php endforeach;?>
-
-<?php
-								$count=1;
-								$ID_PARTICIPANTE = $this->session->userdata('login_user_id');
-
-								$query = "SELECT  SUM(cantidad) as CANTIDAD2 from creditos where ID_PARTICIPANTE = $ID_PARTICIPANTE AND TIPO = 'JUEGO'";
-								$query = $this->db->query($query);
-								$data = $query->result_array();
-
-
-								foreach($data as $row):
-					 ?>
-
-			<div class="col-sm-4 col-xs-6">
-
-				<div class="tile-stats tile-green">
-					<div class="icon">	<img src="<?php echo site_url('../uploads/imagen_creditos_color.png');?>"></div>
-					<div class="num"><?php if($row['CANTIDAD2']>0){echo $row['CANTIDAD2'];} else echo '0';?></div>
-
-					<h3>RETO MENTAL</h3>
-
-				</div>
-
-			</div>
-
-<?php endforeach;?>
-
-<?php
-								$count=1;
-								$ID_PARTICIPANTE = $this->session->userdata('login_user_id');
-
-								$query = "SELECT  SUM(cantidad) as CANTIDAD3 from creditos where ID_PARTICIPANTE = $ID_PARTICIPANTE AND TIPO = 'FACTURA'";
-								$query = $this->db->query($query);
-								$data = $query->result_array();
-
-
-								foreach($data as $row):
-					 ?>
-			<div class="col-sm-4 col-xs-6">
-
-				<div class="tile-stats tile-green">
-					<div class="icon"> <img src="<?php echo site_url('../uploads/imagen_creditos_color.png');?>"> </div>
-					<div class="num"><?php if($row['CANTIDAD3']>0){echo $row['CANTIDAD3'];} else echo '0';?></div>
-
-					<h3>COMPRAS REGISTRADAS</h3>
-
-				</div>
-
-			</div>
-<?php endforeach;?>
-
-				</div>
-			</div>
-
-
-
-			<div class="row">
-				<br><br>
-
-				<H2 align="center" style="color:blue">MANEJO DE CRÉDITOS </H2>
-
-					 <div class="col-sm-6 col-xs-6">
-							<div class="tile-stats tile-blue">
-								<div class="icon"><img src="<?php echo site_url('../uploads/imagen_creditos_gris.png');?>"></div>
-									<div class="num">0</div>
-									<h3>CREDITOS REDIMIDOS: </h3>
-						</div>
-					</div>
-
-
-						 <div class="col-sm-6 col-xs-6">
-								<div class="tile-stats ">
-									<div class="icon"><img src="<?php echo site_url('../uploads/imagen_creditos_gris.png');?>"></div>
-										<div class="num">0</div>
-										<h3>CREDITOS NO REDIMIDOS </h3>
+			<div class="panel" style="margin-top:20px"> 
+				<div class="panel-heading"> 
+					<div class="panel-title"><h2 class="text-info">Datos de usuario</h2></div> 
+				</div> 
+				<div class="panel-body"> 
+					<form role="form" class="form-horizontal form-groups-bordered"> 
+						<div class="form-group"> 
+							<label for="field-1" class="col-sm-3 control-label">NOMBRE</label> 
+							<div class="col-sm-9"> 
+								<input type="text" class="form-control" id="field-1" placeholder="Nombres" value="">
 							</div>
-						</div>
+						</div> 
+						<div class="form-group"> 
+							<label for="field-1" class="col-sm-3 control-label">NOMBRE</label> 
+							<div class="col-sm-9"> 
+								<input type="text" class="form-control" id="field-1" placeholder="Nombres" value="">
+							</div>
+						</div> 				
+						<div class="form-group"> 
+							<div class="col-sm-offset-3 col-sm-5"> 
+								<button type="submit" class="btn btn-default">Sign in</button>
+							</div> 
+						</div> 
+					</form> 
+				</div> 
+			</div>
 
 
+
+
+
+
+
+
+
+		</div>
+		<div class="col-md-6 col-xs-12">
+			<H2 class="text-info">TOTAL CRÉDITOS ACUMULADOS</H2>
+			<div class="row">
+				<div class="col-md-6 col-xs-12">
+					<div class="tile-stats tile-blue">
+						<br style="margin-top:5px;">
+						<div class="icon"><img src="<?php echo site_url('../uploads/imagen_creditos_color.png');?>"></div>
+						<div class="num"><?php if($nCreditos > 0){echo $nCreditos;} else echo '0';?></div>
+						<h3 style="color:#ffffff">TOTAL CREDITOS</h3>
+						<br style="margin-bottom:5px;">
+					</div>
 				</div>
-
+				<div class="col-md-6 col-xs-12">
+					<div class="tile-stats tile-gray" style="padding:10px;">
+						<div class="icon">	<img src="<?php echo site_url('../uploads/imagen_creditos_color.png');?>"></div>
+						<p style="font-size:20px"><?php if($nCreditos2 > 0){echo $nCreditos2;} else echo '0';?></p>
+						<p style="">RETO MENTAL</p>
+					</div>
+					<div class="tile-stats tile-gray" style="padding:10px;">
+						<div class="icon"> <img src="<?php echo site_url('../uploads/imagen_creditos_color.png');?>"></div>
+						<p style="font-size:20px"><?php if($nCreditos3>0){echo $nCreditos3;} else echo '0';?></p>
+						<p style="">COMPRAS REGISTRADAS</p>
+					</div>
+				</div>
+			</div>
+			<p>Número total de créditos acumulados durante la promoción para participar por una consola PlayStation&reg; 4.</p>
+			<hr>
+			<div class="row">
+				<div class="col-md-6 col-xs-12">
+					<H3 class="text-info">CREDITOS REDIMIDOS</H3>
+					<div class="tile-stats tile-aqua">
+						<div class="icon"><img src="<?php echo site_url('../uploads/imagen_creditos_gris.png');?>"></div>
+						<div class="num"><?php if($nCreditos4>0){echo $nCreditos4;} else echo '0';?></div>
+						<h3>CREDITOS REDIMIDOS:</h3>
+					</div>
+				</div>
+				<div class="col-md-6 col-xs-12">
+					<H3 class="text-info">CREDITOS NO REDIMIDOS</H3>
+					<div class="tile-stats tile-aqua">
+						<div class="icon"><img src="<?php echo site_url('../uploads/imagen_creditos_gris.png');?>"></div>
+						<div class="num"><?php if($nCreditos4>0){echo ($nCreditos - $nCreditos4);} else echo '0';?></div>
+						<h3>CREDITOS NO REDIMIDOS</h3>
+					</div>
+				</div>
 			</div>
 		</div>
+	</div>
+</div>
--- application/views/backend/register_account.php
+++ application/views/backend/register_account.php
@@ -1,203 +1,188 @@
-
-
-
 <!doctype html>
 <?php
-//$system_title = $this->db->get_where('settings', array('type' => 'system_title'))->row()->description;
-$system_name  = $this->db->get_where('settings', array('type' => 'system_name'))->row()->description;
+	//$system_title = $this->db->get_where('settings', array('type' => 'system_title'))->row()->description;
+	$system_name  = $this->db->get_where('settings', array('type' => 'system_name'))->row()->description;
+	if (!$_GET['loc']) {
+		echo"<script language='javascript'>window.location='https://elrecreoesdetodos.com/'</script>;";
+	}
 ?>
 
 <html class="no-js" lang="">
-    <head>
-      <meta charset="utf-8">
-      <meta http-equiv="x-ua-compatible" content="ie=edge">
-      <title>
-        <?php echo 'REGISTRARSE'; ?> | <?php echo $system_name; ?>
-      </title>
-      <meta name="description" content="">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-
-      <link rel="shortcut icon" href="<?php echo base_url('assets/login_page/img/favicon.png');?>">
-      <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css');?>">
-      <link rel="stylesheet" href="<?php echo base_url('assets/login_page/css/font-awesome.min.css');?>">
-      <link rel="stylesheet" href="<?php echo base_url('assets/login_page/css/normalize.css');?>">
-      <link rel="stylesheet" href="<?php echo base_url('assets/login_page/css/main.css');?>">
-      <link rel="stylesheet" href="<?php echo base_url('assets/login_page/css/style.css');?>">
-      <script src="<?php echo base_url('assets/login_page/js/vendor/modernizr-2.8.3.min.js');?>"></script>
-		  <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
-
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title><?php echo 'REGISTRARSE'; ?> | <?php echo $system_name; ?></title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="<?php echo base_url('assets/login_page/img/favicon.png');?>">
+		<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css');?>">
+		<link rel="stylesheet" href="<?php echo base_url('assets/login_page/css/font-awesome.min.css');?>">
+		<link rel="stylesheet" href="<?php echo base_url('assets/login_page/css/normalize.css');?>">
+		<link rel="stylesheet" href="<?php echo base_url('assets/login_page/css/main.css');?>">
+		<link rel="stylesheet" href="<?php echo base_url('assets/login_page/css/style.css');?>">
+		<script src="<?php echo base_url('assets/login_page/js/vendor/modernizr-2.8.3.min.js');?>"></script>
+		<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
+		<link rel="stylesheet" href="<?php echo base_url('assets/bootstrap-datepicker/css/bootstrap-datepicker.css');?>" class="">
     </head>
-
 
     <body>
 		<div class="main-content-wrapper">
 			<div class="login-area">
+				
 				<div class="login-header">
-					<a href="<?php echo site_url('login');?>" class="logo">
-						<img src="<?php echo base_url('assets/login_page/img/logo.png');?>"  alt="">
-					</a>
-          <h2 class="title"><?php echo $system_name; ?></h2>
-          <p>Para registrarte debes ser mayor de edad.</p>
+					<img src="<?php echo base_url('assets/login_page/img/logo.png');?>" width="60%" max-width="200px" alt="">
+					<h2 class="title"><?php echo $system_name; ?></h2>
+					<p>* Para registrarte debes ser mayor de edad.</p>
 				</div>
+
+
 				<div class="login-content">
-          <?php echo form_open(site_url('login/register/create') , array('enctype' => 'multipart/form-data'));?>
+					<?php echo form_open(site_url('login/register/create') , array('enctype' => 'multipart/form-data'));?>
+					<div class="form-group">
+						<select name="ID_PAIS" class="form-control selectboxit">
+							<option value=""><?php echo 'SELECIONAR PAIS';?></option>
+							<option value="CO"><?php echo 'COLOMBIA';?></option>
+							<option value="EC"><?php echo 'ECUADOR';?></option>
+							<option value="PE"><?php echo 'PERU';?></option>
+						</select>
+					</div>
 
+					<div class="form-group">
+						<input type="text" class="form-control" name="nombres" 
+						onkeyup="javascript:this.value=this.value.toUpperCase();" 
+						onkeypress="return check2(event)" placeholder="<?php echo 'NOMBRES'?>" 
+						required autocomplete="off">
+					</div>
 
-          <div class="form-group">
+					<div class="form-group">
+						<input type="text" class="form-control" name="apellidos" 
+						onkeyup="javascript:this.value=this.value.toUpperCase();" 
+						onkeypress="return check2(event)" placeholder="<?php echo 'APELLIDOS'?>"
+						required autocomplete="off">
+					</div>
 
-              <select name="ID_PAIS" class="form-control selectboxit">
-                <option value=""><?php echo 'Selecionar pais';?></option>
-                <option value="CO"><?php echo 'COLOMBIA';?></option>
-                <option value="EC"><?php echo 'ECUADOR';?></option>
-                <option value="PE"><?php echo 'PERU';?></option>
+					<div class="form-group">
+						<input type="text" class="form-control" name="identificacion"
+						onkeyup="javascript:this.value=this.value.toUpperCase();" 
+						onkeypress="return check3(event)" placeholder="NUMERO DE DOCUMENTO"
+						required autocomplete="off">
+					</div>
 
-              </select>
-          </div>
+					<div class="form-group">
+						<input type="text" id="dpk" class="form-control" name="ANO_NACIMIENTO" 
+						placeholder="AÑO DE NACIMIENTO">
+					</div>
 
+					<div class="form-group">
+						<input type="text" class="form-control" name="telefono"  
+						onkeypress="return check1(event)" placeholder="TELEFONO CELULAR" 
+						required autocomplete="off">
+					</div>
 
-						<div class="form-group">
-							<input type="text" class="form-control" name="nombres" onkeypress="return check2(event)" placeholder="<?php echo 'NOMBRES'?>"
-                required autocomplete="off">
-						</div>
+					<div class="form-group">
+						<input type="email" class="form-control" name="email" 
+						placeholder="CORREO ELECTRONICO" required autocomplete="off">
+					</div>
 
-            <div class="form-group">
-							<input type="text" class="form-control" name="apellidos" onkeypress="return check2(event)" placeholder="<?php echo 'APELLIDOS'?>"
-                required autocomplete="off">
-						</div>
-
-
-            <div class="form-group">
-                <select name="tipo_documento" class="form-control selectboxit">
-                    <option value=""><?php echo 'Tipo de Documento';?></option>
-                    <option value="CC"><?php echo 'CEDULA DE CIUDADANIA';?></option>
-                    <option value="CE"><?php echo 'CEDULA DE EXTRANJERIA';?></option>
-                    <option value="DNI"><?php echo 'DNI ECUADOR';?></option>
-                    <option value="DNI"><?php echo 'DNI PERU';?></option>
-                </select>
-            </div>
-
-
-
-            <div class="form-group">
-							<input type="text" class="form-control" name="identificacion" onkeypress="return check1(event)" placeholder="<?php echo 'NUMERO DE DOCUMENTO'?>"
-                required autocomplete="off">
-						</div>
-
-
-
-
-            <div class="form-group">
-
-                <input type="text" name="ANO_NACIMIENTO" id="datepicker" class="form-control" placeholder="<?php echo 'FECHA DE NACIMIENTO'?>" autocomplete="off" >
-
-              </div>
-
-              <script type="text/javascript">
-              var maxBirthdayDate = new Date();
-              maxBirthdayDate.setFullYear( maxBirthdayDate.getFullYear() - 18 );
-
-              $( "#datepicker" ).datepicker({
-                dateFormat: 'yy-mm-dd',
-                changeMonth: true,
-                changeYear: true,
-                maxDate: maxBirthdayDate
-              });
-              </script>
-
-            <div class="form-group">
-							<input type="text" class="form-control" name="telefono"  onkeypress="return check1(event)" placeholder="<?php echo 'CELULAR'?>"
-                required autocomplete="off">
-						</div>
-
-            <div class="form-group">
-							<input type="email" class="form-control" name="email" placeholder="<?php echo 'CORREO ELECTRONICO'?>"
-                required autocomplete="off">
-						</div>
-
-
-            <div class="form-group">
-                <?php
-                  $loc = "w";
-                  $loc = $_GET['loc'];
-                  if ($loc != 'w' && $loc == 'co') {
-                    echo '<input type="checkbox" name="aceptar" value="ACEPTO" required> Aceptar <a style="text-decoration: underline; color:#ffffff" href="https://elrecreoesdetodos.com/TYC/dashboard/Colombia-T&C.pdf" target="_blank" style="color:white">terminos y condiciones.</a>';
-                  }
-                  if ($loc != 'w' && $loc == 'ec') {
-                    echo '<input type="checkbox" name="aceptar" value="ACEPTO" required> Aceptar <a style="text-decoration: underline; color:#ffffff" href="https://elrecreoesdetodos.com/TYC/dashboard/Ecuador-T&C.pdf" target="_blank" style="color:white">terminos y condiciones.</a>';
-                  }
-                  if ($loc != 'w' && $loc == 'pe') {
-                    echo '<input type="checkbox" name="aceptar" value="ACEPTO" required> Aceptar <a style="text-decoration: underline; color:#ffffff" href="https://elrecreoesdetodos.com/TYC/dashboard/Peru-T&C.pdf" target="_blank" style="color:white">terminos y condiciones.</a>';
-                  }
-                  if ($loc == 'w') {
-                    echo '<input type="checkbox" name="aceptar" value="ACEPTO" required> Aceptar <a style="text-decoration: underline; color:#ffffff" href="https://elrecreoesdetodos.com/TYC/dashboard/Colombia-T&C.pdf" target="_blank" style="color:white">terminos y condiciones.</a>';
-                  }
-                ?>
-            </div>
-
-
-            <button type="submit" class="btn btn-primary">REGISTRARSE<i class="fa fa-unlock"></i></button>
-            <?php
-              $loc = 'w';
-              $loc = $_GET['loc'];
-              if ($loc != 'w' && $loc == 'co') {
-                echo '<p>¿Ya tienes una cuenta? <a style="color:#ffffff; text-decoration: underline;" href="'.base_url('/?loc=co').'">Inicia sesión</a></p>';
-              }
-              if ($loc != 'w' && $loc == 'ec') {
-                echo '<p>¿Ya tienes una cuenta? <a style="color:#ffffff; text-decoration: underline;" href="'.base_url('/?loc=ec').'">Inicia sesión</a></p>';
-              }
-              if ($loc != 'w' && $loc == 'pe') {
-                echo '<p>¿Ya tienes una cuenta? <a style="color:#ffffff; text-decoration: underline;" href="'.base_url('/?loc=pe').'">Inicia sesión</a></p>';
-              }
-              if ($loc == 'w' || !isset($_GET['loc'])) {
-                echo '<p>¿Ya tienes una cuenta? <a style="color:#ffffff; text-decoration: underline;" href="'.base_url('/?loc=co').'">Inicia sesión</a></p>';
-              }
-            ?>
+					<div class="form-group">
+						<?php
+							$loc = "w";
+							$loc = $_GET['loc'];
+							if ($loc != 'w' && $loc == 'co') {
+								echo '<input type="checkbox" name="aceptar" value="ACEPTO" required> Aceptar <a style="text-decoration: underline; color:#ffffff" href="https://elrecreoesdetodos.com/TYC/dashboard/Colombia-T&C.pdf" target="_blank" style="color:white">terminos y condiciones.</a>';
+							}
+							if ($loc != 'w' && $loc == 'ec') {
+								echo '<input type="checkbox" name="aceptar" value="ACEPTO" required> Aceptar <a style="text-decoration: underline; color:#ffffff" href="https://elrecreoesdetodos.com/TYC/dashboard/Ecuador-T&C.pdf" target="_blank" style="color:white">terminos y condiciones.</a>';
+							}
+							if ($loc != 'w' && $loc == 'pe') {
+								echo '<input type="checkbox" name="aceptar" value="ACEPTO" required> Aceptar <a style="text-decoration: underline; color:#ffffff" href="https://elrecreoesdetodos.com/TYC/dashboard/Peru-T&C.pdf" target="_blank" style="color:white">terminos y condiciones.</a>';
+							}
+						?>
+					</div>
+            		
+					<button type="submit" class="btn btn-primary">REGISTRARSE<i class="fa fa-unlock"></i></button>
+					<?php
+						$loc = 'w';
+						$loc = $_GET['loc'];
+						if ($loc != 'w' && $loc == 'co') {
+							echo '<p>¿Ya tienes una cuenta? <a style="color:#ffffff; text-decoration: underline;" href="'.base_url('/?loc=co').'">Inicia sesión</a></p>';
+						}
+						if ($loc != 'w' && $loc == 'ec') {
+							echo '<p>¿Ya tienes una cuenta? <a style="color:#ffffff; text-decoration: underline;" href="'.base_url('/?loc=ec').'">Inicia sesión</a></p>';
+						}
+						if ($loc != 'w' && $loc == 'pe') {
+							echo '<p>¿Ya tienes una cuenta? <a style="color:#ffffff; text-decoration: underline;" href="'.base_url('/?loc=pe').'">Inicia sesión</a></p>';
+						}
+						if ($loc == 'w' || !isset($_GET['loc'])) {
+							echo '<p>¿Ya tienes una cuenta? <a style="color:#ffffff; text-decoration: underline;" href="'.base_url('/?loc=co').'">Inicia sesión</a></p>';
+						}
+					?>
 				</div>
 			</div>
-
-       <div class="image-area">
-
-
-       </div>
+    		<div class="image-area"></div>
 			<?php echo form_close();?>
 		</div>
 
-    <script src="<?php echo base_url('assets/login_page/js/vendor/jquery-1.12.0.min.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap-notify.js');?>"></script>
-    <script type="text/javascript">
-      function check1(e) {
-          tecla = (document.all) ? e.keyCode : e.which;
+		<script src="<?php echo base_url('assets/login_page/js/vendor/jquery-1.12.0.min.js');?>"></script>
+		<script src="<?php echo base_url('assets/js/bootstrap-notify.js');?>"></script>
+		<script src="<?php echo base_url('assets/bootstrap-datepicker/js/bootstrap-datepicker.js');?>"></script>
+		<script type="text/javascript">
+			function check1(e) {
+				tecla = (document.all) ? e.keyCode : e.which;
 
-          //Tecla de retroceso para borrar, siempre la permite
-          if (tecla == 8) {
-              return true;
-          }
+				//Tecla de retroceso para borrar, siempre la permite
+				if (tecla == 8) {
+					return true;
+				}
 
-          // Patron de entrada, en este caso solo acepta numeros y letras
-          // patron = /[A-Za-z0-9]/;
-          patron = /[0-9]/;
-          tecla_final = String.fromCharCode(tecla);
-          return patron.test(tecla_final);
-        };
-        function check2(e) {
-          tecla = (document.all) ? e.keyCode : e.which;
+				// Patron de entrada, en este caso solo acepta numeros y letras
+				// patron = /[A-Za-z0-9]/;
+				patron = /[0-9]/;
+				tecla_final = String.fromCharCode(tecla);
+				return patron.test(tecla_final);
+			};
+			function check2(e) {
+				tecla = (document.all) ? e.keyCode : e.which;
 
-          //Tecla de retroceso para borrar, siempre la permite
-          if (tecla == 8) {
-              return true;
-          }
+				//Tecla de retroceso para borrar, siempre la permite
+				if (tecla == 8 || tecla  == 32) {
+					return true;
+				}
 
-          // Patron de entrada, en este caso solo acepta numeros y letras
-          // patron = /[A-Za-z0-9]/;
-          patron = /[A-Za-z]/;
-          tecla_final = String.fromCharCode(tecla);
-          return patron.test(tecla_final);
-        };
-    </script>
+				// Patron de entrada, en este caso solo acepta numeros y letras
+				// patron = /[A-Za-z0-9]/;
+				patron = /[A-Za-z]/;
+				tecla_final = String.fromCharCode(tecla);
+				return patron.test(tecla_final);
+			};
+			function check3(e) {
+				tecla = (document.all) ? e.keyCode : e.which;
 
+				//Tecla de retroceso para borrar, siempre la permite
+				if (tecla == 8) {
+					return true;
+				}
 
+				// Patron de entrada, en este caso solo acepta numeros y letras
+				// patron = /[A-Za-z0-9]/;
+				patron = /[A-Za-z0-9]/;
+				tecla_final = String.fromCharCode(tecla);
+				return patron.test(tecla_final);
+			};
+			var maxBirthdayDate = new Date();
+			maxBirthdayDate.setFullYear( maxBirthdayDate.getFullYear() - 18 );
+			
+			$('#dpk').datepicker({
+				language: "es",
+				autoclose: true,
+				toggleActive: true,
+				clearBtn: true,
+				orientation: "auto right",
+				endDate: "31/10/2002"
+			});
+		</script>
     </body>
 </html>

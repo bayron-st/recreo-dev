003c5b81e9636263065fe2bd31ee51a44dd158fc
Bayron St <byr.garcia@gmail.com>
2020-10-11 03:22:54 -0500

    Flujo success
    Se desarrolla flujo success 100% real no fake

--- parejas-memoria/index.php
+++ parejas-memoria/index.php
@@ -13,30 +13,36 @@
     }
 
     if (isset($_POST)) {
-        if (isset($_POST['id_player_losser'])) {
+        if (isset($_POST['go_update_pl'])) {
             $id_player_losser = $_POST['id_player_losser'];
             $id_game_losser = $_POST['id_game_losser'];
             $new_game_count = intval($_POST['new_game_count']) + 1;
             $new_game_shot = intval($_POST['new_game_shot']) + 1;
             $new_game_shot_count = intval($_POST['new_game_shot_count']) - 1;
             $new_game_shot_count2 = intval($_POST['new_game_shot_count2']) - 1;
-
             $date_last_game = date('Y-m-d');
 
-            $query4="UPDATE `game` SET `last_shot` = '$date_last_game', `game_count` = $new_game_count, `game_shot` = $new_game_shot, `game_shot_count` = $new_game_shot_count , `game_shot_count2` = $new_game_shot_count2 WHERE id_participante = $id_player_losser and id_game = $id_game_losser";
+            $query4 = "UPDATE `game` SET `last_shot` = '$date_last_game', `game_count` = $new_game_count, `game_shot` = $new_game_shot, `game_shot_count` = $new_game_shot_count , `game_shot_count2` = $new_game_shot_count2 WHERE id_participante = $id_player_losser and id_game = $id_game_losser";
             $consulta4 = mysqli_query($conexion , $query4);
 
-            if ($consulta4) {
-                echo 'Bien';
-            } else {
-                echo $query4;
-            }
+        } elseif (isset($_POST['go_update_pw'])) {
+            $id_player_winner = $_POST['id_player_winner'];
+            $id_game_winner = $_POST['id_game_winner'];
+            $new_game_level = intval($_POST['new_game_level']) + 1;
+            $new_game_count = intval($_POST['new_game_count']) + 1;
+            $new_game_shot = intval($_POST['new_game_shot']) + 1;
+            $new_game_shot_count = intval($_POST['new_game_shot_count']) - 1;
+            $new_game_shot_count2 = intval($_POST['new_game_shot_count2']) - 1;
+            $date_last_game = date('Y-m-d');
 
-        } elseif (!isset($_POST['id_player_losser'])) {
-            echo 'Nada2';
+            $query5 = "UPDATE `game` SET `game_level` = $new_game_level, `last_shot` = '$date_last_game', `game_count` = $new_game_count, `game_shot` = $new_game_shot, `game_shot_count` = $new_game_shot_count , `game_shot_count2` = $new_game_shot_count2 WHERE id_participante = $id_player_winner and id_game = $id_game_winner";
+            mysqli_query($conexion , $query5);
+
+            $query6 = "INSERT INTO `creditos` (`ID_PARTICIPANTE`, `TIPO`, `CANTIDAD`, `FECHA`, `USUARIO`) VALUES ($id_player_winner, 'JUEGO', '3', '$date_last_game', 'SYSTEM');";
+            mysqli_query($conexion , $query6);
         }
     } elseif (!isset($_POST)) {
-        echo 'Nada1';
+        // echo 'Nada1';
     }
 
 
@@ -82,7 +88,6 @@
             $game_shot = $datos2['game_shot'];
             $game_shot_count = $datos2['game_shot_count'];
             $game_shot_count2 = $datos2['game_shot_count2'];
-
         } elseif (!$consulta2) {
             $id_game = '';
             $game_level = '';
@@ -93,7 +98,6 @@
             $game_shot = '';
             $game_shot_count = '';
             $game_shot_count2 = '';
-
         }
 
         // Consulta la informacion de créditos por juego asociada al jugador
@@ -114,7 +118,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <title>Reta tu memoria</title>
-
         <link rel="stylesheet" href="css/main.css" />
         <link rel="stylesheet" media="(min-width: 1280px) and (max-width: 1400px) and (orientation: landscape)" href="css/mediumWindows.css" />
         <link rel="stylesheet" media="(min-width: 1024px) and (max-width: 1280px) and (orientation: landscape)" href="css/smallWindows.css" />
@@ -134,7 +137,6 @@
     </head>
     <body>
         <div class="container">
-
             <div class="container" style="display:none">
                 <div id="numPartidas" class="numPartidas">
                     Partida nº: <span id="numPartidasValue"></span>
@@ -160,20 +162,21 @@
                         <div class="reloj" id="Minutos">00</div>
                         <div class="reloj" id="Segundos">:00</div>
                     </div>
-                    <div class="well well-sm hidden-xs" style="">Total créditos ganados: <?php echo $nCreditos;?></div>
-                    <div class="well well-sm hidden-xs">Nivel del juego: <?php echo $game_level;?></div>
-                    <div class="well well-sm hidden-xs">Intentos realizados: <?php echo $game_count;?></div>
+                    <div class="well well-sm hidden-xs" style="font-size:20px; color:#333333">Total créditos ganados: <?php echo $nCreditos;?></div>
+                    <div class="well well-sm hidden-xs" style="font-size:20px; color:#333333">Nivel del juego: <?php echo $game_level;?></div>
+                    <div class="well well-sm hidden-xs" style="font-size:20px; color:#333333">Intentos realizados: <?php echo $game_count;?></div>
                 </div>
                 <div class="col-md-8">
                     <div id="wrapper" class="wrapper"></div>
+                    <input type="hidden" id="user_level_game" value="<?php echo $game_level; ?>">
                 </div>
             </div>
             <hr class="visible-xs">
             <div class="row">
                 <div class="col-xs-12 visible-xs" style="margin-top:20px;">
-                    <div class="well well-sm" style="">Total créditos ganados: <?php echo $nCreditos;?></div>
-                    <div class="well well-sm">Nivel del juego: <?php echo $game_level;?></div>
-                    <div class="well well-sm">Intentos realizados: <?php echo $game_count;?></div>
+                    <div class="well well-sm" style="font-size:20px; color:#333333">Total créditos ganados: <?php echo $nCreditos;?></div>
+                    <div class="well well-sm" style="font-size:20px; color:#333333">Nivel del juego: <?php echo $game_level;?></div>
+                    <div class="well well-sm" style="font-size:20px; color:#333333">Intentos realizados: <?php echo $game_count;?></div>
                 </div>
             </div>
         </div>
@@ -193,7 +196,7 @@
                         if ($game_shot_count2 == 2) {$color_texto = '#2c7ea1'; $btn_color = 'btn-info'; $btn_txt_color = '#ffffff';}
                         if ($game_shot_count2 == 3) {$color_texto = '#5cb85c'; $btn_color = 'btn-success'; $btn_txt_color = '#ffffff';}
                     ?>
-                    <p style="font-size:18px;">Te quedan <?php echo '<strong style="color:'.$color_texto.'">'.$game_shot_count2.'</strong>';?> intentos.</p>
+                    <p style="font-size:18px;">Nivel <?php echo '<strong>'.$game_level.'</strong>';?> - Te quedan <?php echo '<strong style="color:'.$color_texto.'">'.$game_shot_count2.'</strong>';?> intentos.</p>
                     <?php
                         if ($game_shot_count2 == 0) {
                             echo '
@@ -206,7 +209,7 @@
                                 <div id="dificultadBtn">
                                     <button type="submit" id="start" class="btn '.$btn_color.' btn-lg" style="font-size: 18px; color:'.$btn_txt_color.'">Iniciar Juego <i class="fa fa-gamepad"></i> </button>
                                 </div>
-                                <p class="text-secondary" style="margin-top:15px">Al iniciar el juego se descontará un intento por semana</p>
+                                <p class="text-secondary" style="margin-top:15px">Al iniciar el juego se descontará un intento.</p>
                             ';
                         }
                     ?>
@@ -224,14 +227,17 @@
                     <h2 style="font-size:30px;">¡Felicidades!</h2>
                     <h3>Has superado el nivel <?php echo $game_level ?></h3>
                     <h3>Ganaste <span style="font-size:20px; color:#5CB85C;"><strong>3</strong></span> nuevos créditos</h3>
-                    <form action="index.php?player=<?php echo $id_player ?>" name="save_score_game" method="POST">
+                    <img src="img/emogis/020-cool-1.png" style="margin: 20px" width="70" height="70">
+                    <form action="index.php?player=<?php echo $id_player ?>" name="update_game_pw" method="POST">
                         <input type="hidden" name="id_player_winner" value="<?php echo $id_player; ?>">
                         <input type="hidden" name="id_game_winner" value="<?php echo $id_game; ?>">
-                        <input type="hidden" name="new_game_count" value="<?php echo $new_game_count; ?>">
+                        <input type="hidden" name="new_game_level" value="<?php echo $game_level; ?>">
+                        <input type="hidden" name="new_game_count" value="<?php echo $game_count; ?>">
                         <input type="hidden" name="new_game_shot" value="<?php echo $game_shot; ?>">
                         <input type="hidden" name="new_game_shot_count" value="<?php echo $game_shot_count; ?>">
-
-                        <button type="submit" id="start" class="btn btn-success btn-lg" style="font-size: 18px;">Siguiente nivel <i class="fa fa-gamepad"></i></button>
+                        <input type="hidden" name="new_game_shot_count2" value="<?php echo $game_shot_count2; ?>">
+                        <a href="https://elrecreoesdetodos.com/dashboard/index.php/participante/dashboard" class="btn btn-info btn-lg" style="font-size: 18px;">Regresar a mi cuenta <i class="fa fa-user"></i></a>
+                        <button name="go_update_pw" type="submit" class="btn btn-success btn-lg" style="font-size: 18px;">Siguiente nivel <i class="fa fa-gamepad"></i></button>
                     </form>
                     <p class="text-secondary" style="margin-top:15px">Al iniciar el juego se descontará un intento por semana</p>
                 </div>
@@ -249,16 +255,15 @@
                     <h3 class="text-danger">Alcanzaste el tiempo límite.</h3>
                     <h3>Te quedan <span style="font-size:20px; color:#AC1818;"><strong><?php echo $game_shot_count;?></strong></span> intentos esta semana.</h3>
                     <img src="img/emogis/085-sad.png" style="margin: 20px" width="70" height="70">
-                    <form action="index.php?player=<?php echo $id_player ?>" name="update_game" method="POST">
+                    <form action="index.php?player=<?php echo $id_player ?>" name="update_game_pl" method="POST">
                         <input type="hidden" name="id_player_losser" value="<?php echo $id_player; ?>">
                         <input type="hidden" name="id_game_losser" value="<?php echo $id_game; ?>">
-                        <input type="hidden" name="new_game_count" value="<?php echo $new_game_count; ?>">
+                        <input type="hidden" name="new_game_count" value="<?php echo $game_count; ?>">
                         <input type="hidden" name="new_game_shot" value="<?php echo $game_shot; ?>">
                         <input type="hidden" name="new_game_shot_count" value="<?php echo $game_shot_count; ?>">
                         <input type="hidden" name="new_game_shot_count2" value="<?php echo $game_shot_count2; ?>">
-
-                        <button type="submit" id="start" class="btn btn-info btn-lg" style="font-size: 18px;">Volver a jugar <i class="fa fa-gamepad"></i></button>
-                        <a href="https://elrecreoesdetodos.com/dashboard/index.php/participante/dashboard" class="btn btn-info btn-lg" style="font-size: 18px; margin-top: 15px;">Regresar a mi cuenta <i class="fa fa-user"></i></a>
+                        <a href="https://elrecreoesdetodos.com/dashboard/index.php/participante/dashboard" class="btn btn-default btn-lg" style="font-size: 18px;">Regresar a mi cuenta <i class="fa fa-user"></i></a>
+                        <button name="go_update_pl" type="submit" class="btn btn-info btn-lg" style="font-size: 18px;">Volver a jugar <i class="fa fa-gamepad"></i></button>
                     </form>
                     <p class="text-secondary" style="margin-top:15px">Al iniciar el juego se descontará un intento.</p>
                 </div>
--- parejas-memoria/js/cronometro.js
+++ parejas-memoria/js/cronometro.js
@@ -1,6 +1,36 @@
-var centesimas = 0;
-var segundos = 10;
-var minutos = 0;
+// var user_level_game = $('#user_level_game').val();
+
+var user_level_game = document.getElementById('user_level_game').value;
+
+if (user_level_game == 1) {
+    var centesimas = 0;
+    var segundos = 50;
+    var minutos = 0;
+    var seg_level = 50;
+    var min_level = 00;
+} else if (user_level_game == 2) {
+    var centesimas = 0;
+    var segundos = 40;
+    var minutos = 0;
+    var seg_level = 40;
+    var min_level = 00;
+} else if (user_level_game == 3) {
+    var centesimas = 0;
+    var segundos = 30;
+    var minutos = 0;
+    var seg_level = 30;
+    var min_level = 00;
+} else {
+    var centesimas = 0;
+    var segundos = 20;
+    var minutos = 0;
+    var seg_level = 20;
+    var min_level = 00;
+}
+
+// var centesimas = 0;
+// var segundos = 10;
+// var minutos = 0;
 var control;
 
 
@@ -23,51 +53,51 @@ function reinicio() {
     this.segundos = 0;
     this.minutos = 0;
 
-    Segundos.innerHTML = ":10";
-    Minutos.innerHTML = "00";
+    Segundos.innerHTML = ":" + seg_level;
+    Minutos.innerHTML = min_level;
 }
 
-             function cronometro () {
+function cronometro() {
 
-                    if((segundos == 60)&&(minutos == -1)){
+    if ((segundos == 60) && (minutos == -1)) {
 
-                    reinicio();
-                    partidaPerdida();
-                }
+        reinicio();
+        partidaPerdida();
+    }
 
-                if(centesimas == 0){
-                        centesimas = 100;
-                }
+    if (centesimas == 0) {
+        centesimas = 100;
+    }
 
-                if(centesimas > 0){
-                    centesimas--;
-                
-                if(centesimas < 10){
-                    centesimas = "0"+centesimas;
-                }
+    if (centesimas > 0) {
+        centesimas--;
 
-                centesimas.innerHTML = ":"+centesimas;
-                 }
+        if (centesimas < 10) {
+            centesimas = "0" + centesimas;
+        }
 
-                if(centesimas == 99){
+        centesimas.innerHTML = ":" + centesimas;
+    }
 
-                     segundos--;
+    if (centesimas == 99) {
 
-                if(segundos < 10){
+        segundos--;
 
-                        segundos = "0"+segundos;
-                }
+        if (segundos < 10) {
 
-                        Segundos.innerHTML = ":"+segundos;
-                }
+            segundos = "0" + segundos;
+        }
 
-                if(segundos == 0){
-                         segundos = 60;
-                }
+        Segundos.innerHTML = ":" + segundos;
+    }
 
-                if((segundos == 60) && (centesimas == 99)){
-                    minutos--;
-                Minutos.innerHTML = minutos;
-                }
+    if (segundos == 0) {
+        segundos = 60;
+    }
 
-            }
+    if ((segundos == 60) && (centesimas == 99)) {
+        minutos--;
+        Minutos.innerHTML = minutos;
+    }
+
+}

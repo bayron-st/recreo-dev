71532c117af54e3a75ba3ea70d08f408b6cfb31f
Andres <dams1801@hotmail.com>
2020-10-12 13:05:02 -0500

    ajustes
    -cambios vistas dashboard

--- application/views/backend/participante/dashboard.php
+++ application/views/backend/participante/dashboard.php
@@ -1,50 +1,91 @@
 
 <html>
 <head>
+    <style>
+
+        .modal-dialog {
+
+            z-index: 10000000 !important;
+        }
+
+    </style>
+    
+
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title></title>
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/fancybox/jquery.fancybox-1.3.4.js');?>"></script>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/js/fancybox/jquery.fancybox-1.3.4.css');?>" media="screen" />
 
-    <script type="text/javascript">
-        $(document).ready(function() {
+                <script>
+                    $(window).on('load',function(){
+                        var delayMs = 1500; // delay in milliseconds
 
-            $("#yt").click(function() {
-                $.fancybox({
-                        'padding'        : 0,
-                        'autoScale'      : false,
-                        'transitionIn'   : 'none',
-                        'transitionOut'  : 'none',
-                        'title'          : this.title,
-                        'width'          : 680,
-                        'height'         : 495,
-                        'href'           : this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
-                        'type'           : 'swf',
-                        'swf'            : {
-                            'wmode'              : 'transparent',
-                            'allowfullscreen'    : 'true'
-                        }
-                    });
-                return false;
-            });
-        });
-        $('#foo').bind('click', function() {
-              alert($(this).text());
-            });
-            $('#foo').trigger('click');
-    </script>
-</head>
+                        setTimeout(function(){
+                            $('#myModal').modal('show');
+                        }, delayMs);
+                    });    
+            </script>
 
-<body onload='$("#yt").trigger("click");'>
+<body>
+<div id="myModal" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Subscribe our Newsletter</h5>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <p>Subscribe to our mailing list to get the latest updates straight in your inbox.</p>
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email Address">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Subscribe</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</body>                <?php
+                                  
+                                  $participante	= $this->db->select('*');
+                                  $participante	= $this->db->from('participantes');
+                                  $participante	= $this->db->where("ID_PARTICIPANTE",$this->session->userdata('login_user_id'));
 
-       
+                                  $participante	= $this->db->get()->result_array();
+
+                                  foreach($participante as $row):?>
+
+
+
+<body>
+
+       <?php  if ($row['ID_PAIS'] == "CO") { ?>
+
         <p><a id="yt" title="" href="https://www.youtube.com/watch?v=0zrb-0BhrWU&feature=emb_logo&ab_channel=ELRECREOESDETODOS"></a></p>
+       
+       <?php }  
+        elseif ($row['ID_PAIS'] == "EC") {?>
+
+        <p><a id="yt" title="" href="https://www.youtube.com/watch?v=zL1awpJ4pg0&feature=youtu.be&ab_channel=ELRECREOESDETODOS"></a></p>
+                
+
+        <?php }  
+                elseif ($row['ID_PAIS'] == "PE") {?>
+
+        <p><a id="yt" title="" href="https://www.youtube.com/watch?v=sLCUs_8y8iY&feature=youtu.be&ab_channel=ELRECREOESDETODOS"></a></p>
+
+                <?php }  ?>
 </body>
 </html>
 
 
+  
+
+<!--<div class="container-fluid" style="border-style: solid; border-color: #02336e; border-width: 15px;">-->
 <div class="container-fluid">
+
 	<div class="row">
 		<div class="col-md-6">
 			<div class="row">
@@ -61,8 +102,20 @@
 		</div>
 		<div class="col-md-6">
 			<div class="row">
-				<div align="center" class="col-md-12"> <br></BR>
-                <img src="<?php echo base_url('uploads/inicio/texto_1.png');?>" >
+                <div align="center" class="col-md-12"> <br></BR>
+
+
+                <?php  if ($row['ID_PAIS'] == "CO") { ?>
+
+                    <img src="<?php echo base_url('uploads/inicio/texto_1.png');?>" >  <?php }  
+                elseif ($row['ID_PAIS'] == "EC") {?>
+
+                    <img src="<?php echo base_url('uploads/inicio/texto_1_ecuador.png');?>" > <?php }  
+                        elseif ($row['ID_PAIS'] == "PE") {?>
+
+                    <img src="<?php echo base_url('uploads/inicio/texto_1_peru.png');?>" ><?php }  ?>
+                                
+                
 					<div class="row">
 						<div class="col-md-12">
                         <img src="<?php echo base_url('uploads/inicio/premios-bienvenida_2celulares.jpg');?>" >
@@ -76,3 +129,5 @@
 		</div>
 	</div>
 </div>
+
+<?php endforeach;?>
--- application/views/backend/participante/participa.php
+++ application/views/backend/participante/participa.php
@@ -5,25 +5,61 @@
 				<div class="col-md-12">
 					<div class="row">
                         <br><br>
+
+                                <?php
+                                  
+                                  $participante	= $this->db->select('*');
+                                  $participante	= $this->db->from('participantes');
+                                  $participante	= $this->db->where("ID_PARTICIPANTE",$this->session->userdata('login_user_id'));
+
+                                  $participante	= $this->db->get()->result_array();
+
+                                  foreach($participante as $row):?>
+
+
+                        
                     <center> <img  src="<?php echo base_url('uploads/participa/texto_1.png');?>" > </center> <br><br>
-						<div class="col-md-12">
-                             <iframe width="615" height="350" src="https://www.youtube.com/embed/0zrb-0BhrWU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-						</div>
+                    
+                            <div class="col-md-12">
+
+                    <?php  if ($row['ID_PAIS'] == "CO") { ?>
+                             <iframe width="100%" height="350" src="https://www.youtube.com/embed/0zrb-0BhrWU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <?php }  elseif ($row['ID_PAIS'] == "EC") {?>
+                             <iframe width="100%" height="350" src="https://www.youtube.com/embed/zL1awpJ4pg0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <?php }  elseif ($row['ID_PAIS'] == "PE") {?>       
+                             <iframe width="100%" height="350" src="https://www.youtube.com/embed/sLCUs_8y8iY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <?php }  ?>      
+
+                            </div>
 					</div>
 				</div>
 			</div>
-		</div>
+        </div>
+        
+
 		<div class="col-md-6">
 			<div class="row">
 
+            
                <center> <img  src="<?php echo base_url('uploads/participa/texto_2.png');?>" > </center> <br><br>
                     <div align="center" class="col-md-12">
-                         <img src="<?php echo base_url('uploads/participa/texto_3.png');?>" >
+
+                    <?php  if ($row['ID_PAIS'] == "CO") { ?>
+                        <img src="<?php echo base_url('uploads/participa/texto_3.png');?>" >
                          <img src="<?php echo base_url('uploads/participa/texto_4.png');?>" >
+                    <?php }  elseif ($row['ID_PAIS'] == "EC") {?>
+                        <img src="<?php echo base_url('uploads/participa/texto_3_ecuador.png');?>" >
+                         <img src="<?php echo base_url('uploads/participa/texto_4_ecuador.png');?>" >
+                    <?php }  elseif ($row['ID_PAIS'] == "PE") { ?>       
+                        <img src="<?php echo base_url('uploads/participa/texto_3_peru.png');?>" >
+                         <img src="<?php echo base_url('uploads/participa/texto_4_peru.png');?>" >
+                    <?php }  ?>   
+            
+                    <?php endforeach;?>
                          <img src="<?php echo base_url('uploads/participa/texto_5.png');?>" >
                          <img src="<?php echo base_url('uploads/participa/texto_6.png');?>" >
                     </div>
              </div>
 		</div>
 	</div>
-</div>
+</div>      

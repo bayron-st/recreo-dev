ddedcd931ab1afd49f980d2d3eb2e082453edf87
Andres <dams1801@hotmail.com>
2020-10-12 19:53:57 -0500

    menu
    ajustes menu

--- application/views/backend/participante/navigation.php
+++ application/views/backend/participante/navigation.php
@@ -1,81 +1,69 @@
-
-	<header class="navbar"><!-- set fixed position by adding class "navbar-fixed-top" -->
-
-		<div class="navbar-inner">
-			<!-- main menu -->
-			<div class="navbar-brand">
+<nav class="navbar navbar-default" role="navigation">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			
 			</div>
-			<?php
-
-				$count=1;
-				$ID_PARTICIPANTE = $this->session->userdata('login_user_id');
-
-				$query = "SELECT id_pais from participantes where ID_PARTICIPANTE = $ID_PARTICIPANTE ";
-				$query = $this->db->query($query);
-				$data = $query->result_array();
-
-				$loc = '';
-				$loc2 = '';
-
-				foreach($data as $row):
-
-				if($row['id_pais'] == 'CO') {$loc = "co"; $loc2 = "";}
-				if($row['id_pais'] == 'EC') {$loc = "ec"; $loc2 = "-ec";}
-				if($row['id_pais'] == 'PE') {$loc = "pe"; $loc2 = "-pe";}
-			?>
-
-			<ul class="navbar-nav pull-sm-left">
-				
-
-				<li class="<?php if ($page_name == 'dashboard') echo 'active'; ?> ">
-					<a href="<?php echo site_url('participante/dashboard'); ?>">
-						<span>inicio <i class="fa fa-file"></i></span>
-					</a>
-				</li> 
-
-				<li class="<?php if ($page_name == 'participa') echo 'active'; ?> ">
-					<a href="<?php echo site_url('participante/participa'); ?>">
-						<span>Cómo participar <i class="fa fa-file"></i></span>
-					</a>
-				</li> 
-
-				<li class="<?php if ($page_name == 'juego') echo 'active'; ?> ">
-					<a href="<?php echo site_url('participante/juego'); ?>">
-						<span>Juego <i class="fa fa-gamepad"></i></span>
-					</a>
-				</li>
-
-				<li class="<?php if ($page_name == 'premios') echo 'active'; ?> ">
-					<a href="<?php echo site_url('participante/premios'); ?>">
-						<span>Premios <i class="fa fa-gamepad"></i></span>
-					</a>
-				</li>
-					
 		
-				<li class="<?php if ($page_name == 'whatsapp') echo 'active'; ?> ">
-					<a href="<?php echo site_url('participante/whatsapp'); ?>">
-						<span>Registrar Compras <i class="fa fa-file"></i></span>
-					</a>
-				</li>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="font-size:16px !important;">
+				<ul class="nav navbar-nav">
 
-				<li class="<?php if ($page_name == 'redimir') echo 'active'; ?> ">
-					<a href="<?php echo site_url('participante/redimir'); ?>">
-						<span>Redimir <i class="fa fa-tags"></i></span>
-					</a>
-				</li> 
 
-				<li class="has-sub <?php if ($page_name == 'cuenta') echo 'active'; ?>" >
-					<a href="<?php echo site_url('participante/cuenta'); ?>">
-						<span class="title">Mi cuenta <i class="fa fa-user"></i></span>
-					</a>
-				</li>
-			</ul>
+					<?php
+						$count=1;
+						$ID_PARTICIPANTE = $this->session->userdata('login_user_id');
 
-			<?php endforeach;?>
-			<!-- notifications and other links -->
-			<ul class="nav navbar-right pull-right">
+						$query = "SELECT id_pais from participantes where ID_PARTICIPANTE = $ID_PARTICIPANTE ";
+						$query = $this->db->query($query);
+						$data = $query->result_array();
 
-				<li class="has-sub">
+						$loc = '';
+						$loc2 = '';
+
+						foreach($data as $row):
+
+						if($row['id_pais'] == 'CO') {$loc = "co"; $loc2 = "";}
+						if($row['id_pais'] == 'EC') {$loc = "ec"; $loc2 = "-ec";}
+						if($row['id_pais'] == 'PE') {$loc = "pe"; $loc2 = "-pe";}
+					?>
+
+						<li class="<?php if ($page_name == 'dashboard') echo 'active'; ?> ">
+							<a href="<?php echo site_url('participante/dashboard'); ?>">
+								<span >Inicio <i class="fa fa-file"></i></span>
+							</a>
+						</li> 
+
+						<li class="<?php if ($page_name == 'participa') echo 'active'; ?> ">
+							<a href="<?php echo site_url('participante/participa'); ?>">
+								<span>Cómo participar <i class="fa fa-file"></i></span>
+							</a>
+						</li> 
+
+						<li class="<?php if ($page_name == 'juego') echo 'active'; ?> ">
+							<a href="<?php echo site_url('participante/juego'); ?>">
+								<span>Juego <i class="fa fa-gamepad"></i></span>
+							</a>
+						</li>
+
+						<li class="<?php if ($page_name == 'premios') echo 'active'; ?> ">
+							<a href="<?php echo site_url('participante/premios'); ?>">
+								<span>Premios <i class="fa fa-gamepad"></i></span>
+							</a>
+						</li>
+		
+				</ul>
+				<?php endforeach;?>
+
+				<ul class="nav navbar-nav navbar-right" style="font-size:16px !important;">
+					
+					<li class="dropdown">
+
 					<?php
 						$ID_PARTICIPANTE = $this->session->userdata('login_user_id');
 						$query = "SELECT NOMBRES  from 	participantes where ID_PARTICIPANTE = $ID_PARTICIPANTE ";
@@ -83,32 +71,41 @@
 						$data = $query->result_array();
 						foreach($data as $row):
 					?>
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-close-others="true">
-						<span class="title">
-							<?php echo $row['NOMBRES'];endforeach;?>
-						</span>
-                	</a>
-				</li>
 
-				<li class="has-sub">
-					<a href="<?php echo site_url('login/logout');?>">
-					<span class="title" style="font-size:16px !important;">Cerrar Sesión</span><i class="entypo-logout right"></i>
-					</a>
-				</li>
-				<!-- mobile only -->
-				<li class="visible-xs">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"> <?php echo $row['NOMBRES'];endforeach;?> <b class="caret"></b></a>
+						<ul class="dropdown-menu" style="font-size:16px !important;">
+						<li  <?php if ($page_name == 'cuenta') echo 'active'; ?>">
+								<a href="<?php echo site_url('participante/cuenta'); ?>">
+									<span class="title"> Mi cuenta <i class="fa fa-user"> </i> </span>
+								</a>
+							</li>
 
-					<!-- open/close menu icon (do not remove if you want to enable menu on mobile devices) -->
-					<div class="horizontal-mobile-menu visible-xs">
-						<a href="#" class="with-animation"><!-- add class "with-animation" to support animation -->
-							<i class="entypo-menu"></i>
-						</a>
-					</div>
+							<li class="<?php if ($page_name == 'redimir') echo 'active'; ?> ">
+								<a href="<?php echo site_url('participante/redimir'); ?>">
+									<span>Redimir <i class="fa fa-tags"></i></span>
+								</a>
+							</li> 
 
-				</li>
+							<li class="<?php if ($page_name == 'whatsapp') echo 'active'; ?> ">
+								<a href="<?php echo site_url('participante/whatsapp'); ?>">
+									<span>Registrar Compras <i class="fa fa-file"></i></span>
+								</a>
+							</li>
 
-			</ul>
+							<li class="has-sub">
+								<a href="<?php echo site_url('login/logout');?>">
+								<span class="title" >Cerrar Sesión</span><i class="entypo-logout right"></i>
+								</a>
+							</li>
+	
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</nav>
+	
 
-		</div>
 
-	</header>
+
+	

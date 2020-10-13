bd727357668f27634f49ee071f2547c325bc56d4
Bayron St <byr.garcia@gmail.com>
2020-10-13 02:28:27 -0500

    Ajuste de logica y estilos
    Se ajustan las vistas de redimir y premios

--- application/views/backend/participante/premios.php
+++ application/views/backend/participante/premios.php
@@ -5,18 +5,19 @@
         <img class="img-responsive center-block" style="margin-bottom: 10px;" src="https://i2.wp.com/elrecreoesdetodos.com/wp-content/uploads/2020/10/texto_2-2.png?w=325&ssl=1" alt="">
         <img class="img-responsive center-block" style="margin-bottom: 10px;" src="https://i0.wp.com/elrecreoesdetodos.com/wp-content/uploads/2020/10/premios-playstation4.jpg?w=324&ssl=1" alt="">
         <img class="img-responsive center-block" style="margin-bottom: 10px;" src="https://i1.wp.com/elrecreoesdetodos.com/wp-content/uploads/2020/10/texto_5-1.png?w=315&ssl=1" alt="">
-        <button class="btn btn-lg btn-blue" style="font-size:20px; margin: 10px 0px; width:250px" data-toggle="modal" data-target="#myModal1">VER RANKIN <i class="fa fa-trophy"></i></button>
+        <button class="btn btn-lg btn-blue disabled" style="font-size:20px; margin: 10px 0px; width:250px" data-toggle="modal" data-target="#myModal1">VER RANKIN <i class="fa fa-trophy"></i></button>
+        <p style="font-size:18px;">Próximamente</p>
     </div>
     <div class="col-md-4 text-center" style="padding-top: 10px; background-color: #75BEE9;">
         <img class="img-responsive center-block" style="margin-bottom: 10px;" src="https://i1.wp.com/elrecreoesdetodos.com/wp-content/uploads/2020/10/texto_3-1.png?w=326&ssl=1" alt="">
         <img class="img-responsive center-block" style="margin-bottom: 10px;" src="https://i0.wp.com/elrecreoesdetodos.com/wp-content/uploads/2020/10/premios-netflix_spotify.jpg?w=324&ssl=1" alt="">
         <img class="img-responsive center-block" style="margin-bottom: 10px;" src="https://i2.wp.com/elrecreoesdetodos.com/wp-content/uploads/2020/10/texto_6-1.png?fit=274%2C55&ssl=1" alt="">
-        <a href="#" class="btn btn-lg btn-blue" style="font-size:20px; margin: 30px 0px; width:250px">REDIMIR PREMIOS <i class="fa fa-tags"></i></a href="#">
+        <a href="<?php echo site_url('/participante/redimir') ?>" class="btn btn-lg btn-blue" style="font-size:20px; margin: 30px 0px; width:250px">REDIMIR PREMIOS <i class="fa fa-tags"></i></a href="#">
     </div>
     <div class="col-md-4 text-center" style="padding-top: 20px;">
         <img class="img-responsive center-block" style="margin-bottom: 10px;" src="https://i2.wp.com/elrecreoesdetodos.com/wp-content/uploads/2020/10/texto_4-1.png?w=325&ssl=1" alt="">
         <img class="img-responsive center-block" style="margin-bottom: 10px;" src="https://i0.wp.com/elrecreoesdetodos.com/wp-content/uploads/2020/10/Premios-tipo-C.jpg?w=340&ssl=1" alt="">
         <img class="img-responsive center-block" style="margin-bottom: 10px;" src="https://i0.wp.com/elrecreoesdetodos.com/wp-content/uploads/2020/10/texto_7.png?w=304&ssl=1" alt="">
-        <a href="#" class="btn btn-lg btn-blue" style="font-size:20px; margin: 10px 0px; width:250px">REDIMIR PREMIOS <i class="fa fa-tags"></i></a href="#">
+        <a href="<?php echo site_url('/participante/redimir') ?>" class="btn btn-lg btn-blue" style="font-size:20px; margin: 10px 0px; width:250px">REDIMIR PREMIOS <i class="fa fa-tags"></i></a href="#">
     </div>
 </div>

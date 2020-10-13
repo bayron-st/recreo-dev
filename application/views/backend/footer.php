a739426dddf3ebf45dc6345e08a922a8916ae94d
Andres <dams1801@hotmail.com>
2020-10-13 03:08:07 -0500

    ajustes
    footer

--- application/views/backend/footer.php
+++ application/views/backend/footer.php
@@ -1,4 +1,85 @@
 <!-- Footer -->
-<footer class="main">
-    &copy; 2020
+<footer class="main" style="background-color: #CCCCCC; padding: 25px 15px;">
+<div class="container-fluid" >
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-3">
+                 <img class="img-responsive" src="<?php echo base_url('uploads/logo_header.png'); ?>" />
+				</div>
+				<div align="center" class="col-md-3">
+
+                <?php 
+        $participante	= $this->db->select('*');
+        $participante	= $this->db->from('participantes');
+        $participante	= $this->db->where("ID_PARTICIPANTE",$this->session->userdata('login_user_id'));
+        $participante	= $this->db->get()->result_array();
+
+        foreach($participante as $row):?>
+
+        <?php  if ($row['ID_PAIS'] == "CO") { ?>     
+                            <a style="font-size:18px !important;" href="https://elrecreoesdetodos.com/TYC/Colombia-T&amp;C.pdf" target="_blank">Terminos y Condiciones</a> <br>
+                            <a style="font-size:18px !important;"href="https://www.tetrapak.com/co/about/legal-information" target="_blank">Políticas de privacidad</a> <br>
+                            <a style="font-size:18px !important;" href="https://elrecreoesdetodos.com/TYC/Usabilidad_de_plataforma_web.pdf">Sobre el uso de la plataforma</a> 
+       <?php }  elseif ($row['ID_PAIS'] == "EC") {?>
+                            <a style="font-size:18px !important;" href="https://elrecreoesdetodos.com/TYC/Ecuador-T&amp;C.pdf" target="_blank">Terminos y Condiciones</a>        
+                            <a style="font-size:18px !important;" href="https://www.tetrapak.com/ec/about/legal-information" target="_blank">Políticas de privacidad</a>
+                            <a style="font-size:18px !important;" href="https://elrecreoesdetodos.com/TYC/Usabilidad_de_plataforma_web.pdf">Sobre el uso de la plataforma</a>
+                            <?php }   elseif ($row['ID_PAIS'] == "PE") {?>
+                            <a style="font-size:18px !important;" href="https://elrecreoesdetodos.com/TYC/Peru-T&amp;C.pdf" target="_blank">Terminos y Condiciones</a>
+                            <a style="font-size:18px !important;" href="https://www.tetrapak.com/pe/about/legal-information" target="_blank">Políticas de privacidad</a>
+                            <a style="font-size:18px !important;"href="https://elrecreoesdetodos.com/TYC/Usabilidad_de_plataforma_web.pdf">Sobre el uso de la plataforma</a>
+        <?php }  ?>
+                       
+                                            
+				</div>
+				<div class="col-md-3 text-center">
+                    <p>Estas navegando para</p>
+
+                    <?php  if ($row['ID_PAIS'] == "CO") { ?>     
+                        <img style="width: 70px;" class="img-responsive center-block" src="<?php echo base_url('uploads/banderas/colombia.png'); ?>" />                         
+                    <?php }  elseif ($row['ID_PAIS'] == "EC") {?>
+                        <img class="img-responsive center-block" src="<?php echo base_url('uploads/banderas/ecuador.png'); ?>" />  
+                    <?php }   elseif ($row['ID_PAIS'] == "PE") {?>
+                        <img class="img-responsive center-block" src="<?php echo base_url('uploads/banderas/peru.png'); ?>" />  
+                    <?php }  ?>
+				</div>
+				<div class="col-md-3">
+
+                <?php  if ($row['ID_PAIS'] == "CO") { ?>     
+                    <a target="_blank" style="width: 100%;" href="https://www.rappi.com.co/tiendas/exito/mundo-tetra-pak?_branch_match_id=843950165437867036" type="button" class="btn btn-orange"> Compra y recibe a domicilio <i class="fa fa-bicycle"></i> </a>                        
+                    <?php }  elseif ($row['ID_PAIS'] == "EC") {?>
+                        <a target="_blank" style="width: 100%;" href="https://www.rappi.com.ec/tiendas/supermaxi/tetra-pak?_branch_match_id=843950165437867036" type="button" class="btn btn-orange"> Compra y recibe a domicilio <i class="fa fa-bicycle"></i> </a> 
+                    <?php }   elseif ($row['ID_PAIS'] == "PE") {?>
+                        <a target="_blank" style="width: 100%;" href="https://www.rappi.com.pe/tiendas/wong/mundo-tetra-pak?_branch_match_id=843950165437867036" type="button" class="btn btn-orange"> Compra y recibe a domicilio <i class="fa fa-bicycle"></i> </a> 
+                    <?php }  ?>
+                    
+                    <div class="text-center"style="margin-top: 15px;"> 
+                    
+                    <?php  if ($row['ID_PAIS'] == "CO") { ?>     
+                        <a href="https://www.facebook.com/TetraPakColombia/" style="margin: 3px;" ><i class="fa fa-facebook fa-3x text-info"></i></a>      
+                    <?php }  elseif ($row['ID_PAIS'] == "EC") {?>
+                        <a href="http://www.facebook.com/tetrapakecuador" style="margin: 3px;" ><i class="fa fa-facebook fa-3x text-info"></i></a>      
+                         
+                    <?php }   elseif ($row['ID_PAIS'] == "PE") {?>
+                        <a href=" http://www.facebook.com/tetrapakperu" style="margin: 3px;" ><i class="fa fa-facebook fa-3x text-info"></i></a>      
+                    <?php }  ?>
+                   
+                        <a href="https://www.instagram.com/TetraPakAndina/" style="margin: 3px;" ><i class="fa fa-instagram fa-3x text-info"></i></a>
+                        <a href="https://www.youtube.com/channel/UChbiQa6dP4GNzsp1CNUBX2w" style="margin: 3px;" ><i class="fa fa-youtube-play fa-3x text-info"></i></a>
+                        <a href="http://twitter.com/TetraPakAndina" style="margin: 3px;" ><i class="fa fa-twitter fa-3x text-info"></i></a>
+                        <a href="http://linkedin.com/company/tetra-pak/" style="margin: 3px;" ><i class="fa fa-linkedin fa-3x text-info"></i></a>
+                    </div>
+
+
+
+
+
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 </footer>
+<?php endforeach;?>
--- application/views/backend/includes_bottom.php
+++ application/views/backend/includes_bottom.php
@@ -1,8 +1,129 @@
+<link href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.css" rel="stylesheet" />
+<link href="//cdn.datatables.net/buttons/1.2.2/css/buttons.dataTables.css" rel="stylesheet" />
 
-<!-- Bottom Scripts -->
-<script src="<?php echo base_url('assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js');?>"></script>
-<script src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
+<script src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="//cdn.datatables.net/buttons/1.4.2/js/dataTables.buttons.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+<script src="//cdn.datatables.net/buttons/1.4.2/js/buttons.html5.min.js"></script>
+
+
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js"></script>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.45/js/bootstrap-datetimepicker.min.js"></script>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.45/css/bootstrap-datetimepicker.min.css" />
+
+<link rel="stylesheet" href="<?php echo base_url('assets/js/daterangepicker/daterangepicker-bs3.css');?>">
+
+<script src="<?php echo base_url('assets/js/bootstrap-timepicker.min.js');?>"></script>
+
+
+<link rel="stylesheet" href="<?php echo base_url('assets/js/datatables/responsive/css/datatables.responsive.css');?>">
+<link rel="stylesheet" href="<?php echo base_url('assets/js/select2/select2-bootstrap.css');?>">
+<link rel="stylesheet" href="<?php echo base_url('assets/js/select2/select2.css');?>">
+<link rel="stylesheet" href="<?php echo base_url('assets/js/selectboxit/jquery.selectBoxIt.css');?>">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/colores.css');?>">
+<!-- fecha rangos -->
+
+<script src="<?php echo base_url('assets/js/daterangepicker/moment.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/daterangepicker/daterangepicker.js');?>"></script>
+
+
+
+
+	<!-- Bottom Scripts -->
 <script src="<?php echo base_url('assets/js/gsap/main-gsap.js');?>"></script>
+<script src="<?php echo base_url('assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js');?>"></script>
+
+
+<script src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
+<script src="<?php echo base_url('assets/js/joinable.js');?>"></script>
+<script src="<?php echo base_url('assets/js/resizeable.js');?>"></script>
+<script src="<?php echo base_url('assets/js/neon-api.js');?>"></script>
+<script src="<?php echo base_url('assets/js/toastr.js');?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.validate.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/fullcalendar/fullcalendar.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/bootstrap-datepicker.js');?>"></script>
+<script src="<?php echo base_url('assets/js/fileinput.js');?>"></script>
+
+<script src="<?php echo base_url('assets/js/jquery.dataTables.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/datatables/TableTools.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/dataTables.bootstrap.js');?>"></script>
+<script src="<?php echo base_url('assets/js/datatables/jquery.dataTables.columnFilter.js');?>"></script>
+<script src="<?php echo base_url('assets/js/datatables/lodash.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/datatables/responsive/js/datatables.responsive.js');?>"></script>
+<script src="<?php echo base_url('assets/js/select2/select2.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/selectboxit/jquery.selectBoxIt.min.js');?>"></script>
+
+
+<script src="<?php echo base_url('assets/js/neon-calendar.js');?>"></script>
+<script src="<?php echo base_url('assets/js/neon-chat.js');?>"></script>
+<script src="<?php echo base_url('assets/js/neon-custom.js');?>"></script>
+<script src="<?php echo base_url('assets/js/neon-demo.js');?>"></script>
+
 <script src="<?php echo base_url('assets/js/wysihtml5/wysihtml5-0.4.0pre.min.js');?>"></script>
 <script src="<?php echo base_url('assets/js/wysihtml5/bootstrap-wysihtml5.js');?>"></script>
 
+
+
+
+
+<!-- SHOW TOASTR NOTIFIVATION -->
+<?php if ($this->session->flashdata('flash_message') != ""):?>
+
+<script type="text/javascript">
+	toastr.success('<?php echo $this->session->flashdata("flash_message");?>');
+</script>
+
+<?php endif;?>
+
+<?php if ($this->session->flashdata('error_message') != ""):?>
+
+	<script>
+															 swal.fire({
+																   type: 'error',
+																	 title: 'Oops...',
+																	 text: "<?php echo $this->session->flashdata('error_message'); ?>",
+																	 timer: 10000,
+																	 position: 'center',
+																	 showConfirmButton: false,
+																	 type: 'error'
+															 });
+
+													 </script>
+
+<?php endif;?>
+
+
+
+
+<!---  DATA TABLE EXPORT CONFIGURATIONS -->
+<script type="text/javascript">
+
+	jQuery(document).ready(function($)
+	{
+		var datatable = $("#table_export").dataTable();
+
+		$(".dataTables_wrapper select").select2({
+			minimumResultsForSearch: -1
+		});
+	});
+
+</script>
+
+
+<script type="text/javascript">
+		function format(input)
+		{
+		var num = input.value.replace(/\./g,'');
+		if(!isNaN(num)){
+		num = num.toString().split('').reverse().join('').replace(/(?=\d*\.?)(\d{3})/g,'$1.');
+		num = num.split('').reverse().join('').replace(/^[\.]/,'');
+		input.value = num;
+		}
+
+		else{ alert('Solo se permiten numeros');
+		input.value = input.value.replace(/[^\d\.]*/g,'');
+		}
+		}
+</script>
